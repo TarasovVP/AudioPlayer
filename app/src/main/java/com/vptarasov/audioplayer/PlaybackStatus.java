a8a8b3b687ba4dcb277e1136d5706eb26b60264d
TarasovVP <vptarasov1998@gmail.com>
2019-07-09 09:51:01 +0300

    Initial commit

---
+++ app/src/main/java/com/vptarasov/audioplayer/PlaybackStatus.java
@@ -0,0 +1,6 @@
+package com.vptarasov.audioplayer;
+
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
---
+++ app/src/main/java/com/vptarasov/audioplayer/onItemClickListener.java
@@ -0,0 +1,8 @@
+package com.vptarasov.audioplayer;
+
+import android.view.View;
+
+public interface onItemClickListener {
+
+    void onClick(View view, int index);
+}
